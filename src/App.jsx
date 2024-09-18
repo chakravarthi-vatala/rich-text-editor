346c8fa1217870ec0f6973d5b30111f537626a4e
Chakravarthi Vatala <chakravarthi.vatala@italentdigital.com>
2024-09-18 18:28:57 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import TextEditor from "./TextEditor";
+function App() {
+  return (
+    <div id="editor1" height="100%">
+      <TextEditor height="100%" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TextEditor.jsx
@@ -0,0 +1,86 @@
+import { env, pipeline } from "@xenova/transformers";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { useEffect, useRef, useState } from "react";
+
+export default function TextEditor() {
+  env.allowLocalModels = false;
+  env.useBrowserCache = false;
+  const editorRef = useRef(null);
+  const quillInstance = useRef(null);
+  const classifierRef = useRef(null); // Store the classifier once it's loaded
+  let [label, setLabel] = useState([]);
+  let [score, setScore] = useState([]);
+
+  useEffect(() => {
+    if (editorRef.current && !quillInstance.current) {
+      quillInstance.current = new Quill(editorRef.current, {
+        debug: "info",
+        modules: {
+          toolbar: true,
+        },
+        placeholder: "Type here....",
+        theme: "snow",
+      });
+    }
+    document.getElementById("label").style.display = "none";
+  }, []);
+
+  useEffect(() => {
+    async function loadModel() {
+      try {
+        console.log("Loading the model, please be patient");
+        const classifier = await pipeline(
+          "sentiment-analysis",
+          "Xenova/distilbert-base-uncased-finetuned-sst-2-english"
+        );
+        classifierRef.current = classifier; // Store the loaded classifier
+      } catch (error) {
+        console.error("Error loading the model:", error);
+      }
+    }
+
+    // Load the model once
+    if (!classifierRef.current) {
+      loadModel();
+    }
+
+    const handleTextChange = async () => {
+      setTimeout(async () => {
+        const editorContent = quillInstance.current.getText();
+        if (classifierRef.current) {
+          try {
+            const output = await classifierRef.current(editorContent);
+            setLabel(output[0].label);
+            setScore(output[0].score);
+            document.getElementById("label").style.display = "block";
+          } catch (error) {
+            console.error("Error classifying text:", error);
+          }
+        }
+      }, 5000);
+    };
+
+    if (quillInstance.current) {
+      quillInstance.current.on("text-change", handleTextChange);
+    }
+
+    // Clean up the event listener
+    return () => {
+      if (quillInstance.current) {
+        quillInstance.current.off("text-change", handleTextChange);
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <div ref={editorRef} id="editor" style={{ height: "397px" }}></div>
+      <div id="label">
+        <b>Label:{label}</b>
+        <br />
+        <b>Score:{score}</b>
+      </div>
+    </>
+  );
+}
