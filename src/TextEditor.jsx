86476450ec230f42962c60868c435e99b63f62e6
Chakravarthi Vatala <chakravarthi.vatala@italentdigital.com>
2024-09-19 15:18:16 +0530

    update 4

--- src/TextEditor.jsx
+++ src/TextEditor.jsx
@@ -70,7 +70,7 @@ export default function TextEditor() {
             console.error("Error classifying text:", error);
           }
         }
-      }, 5000);
+      }, 3000);
     };
 
     if (quillInstance.current) {
